c54dc9d03e302ea93d40a08f176a2add845103ac
Loai Ghoraba <loai1991@gmail.com>
2016-12-11 16:26:20 +0200

    adding versionist and devise

--- fleeto_backend/config/routes.rb
+++ fleeto_backend/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+
+    namespace :customer do
+    end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
