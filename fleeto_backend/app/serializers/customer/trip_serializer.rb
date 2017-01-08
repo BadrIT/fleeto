4205579a0acc72394477730eeaf52cd84cfab776
Mohamed Elsaka <mohamed.elsaka2007@gmail.com>
2017-01-08 10:34:22 +0100

    Fix trips serializers

--- fleeto_backend/app/serializers/customer/trip_serializer.rb
+++ fleeto_backend/app/serializers/customer/trip_serializer.rb
@@ -1,3 +1,4 @@
 class Customer::TripSerializer < ActiveModel::Serializer
-  attributes :id, :driver_id, :from_long, :from_lat, :to_long, :to_lat, :started_at, :ended_at, :status
+  attributes :id, :driver_id, :from_longitude, :from_latitude,
+    :to_longitude, :to_latitude, :started_at, :ended_at, :status
 end
